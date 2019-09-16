5d06efd57ef5601b2cd9bbe337f311a3b50b75c2
Claire Cassidy <clacassidy@gmail.com>
2019-09-16 16:43:27 -0700

    neon header and body start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextColor from './components/TextColor';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextColor />
       </header>
+      <body className="App-body">
+        <h3>
+          Fullstack Software Developer
+        </h3>
+        <div className="fuck">
+        <p>Projects <a
+          className="App-link"
+          href="https://twitter.com/AlchemyPDXBot"
+          target="_blank"
+          rel="noopener noreferrer">
+          here, 
+        </a> <a
+          className="App-link"
+          href="https://twitter.com/AlchemyPDXBot"
+          target="_blank"
+          rel="noopener noreferrer">
+          here, 
+        </a> and <a
+          className="App-link"
+          href="https://twitter.com/AlchemyPDXBot"
+          target="_blank"
+          rel="noopener noreferrer">
+          here, 
+        </a>
+        </p>
+          
+        </div>
+      </body>
     </div>
   );
 }
