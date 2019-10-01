c54a1eda945d969fe5fcae5bd5b0e76569ea03da
Claire Cassidy <clacassidy@gmail.com>
2019-10-01 10:24:09 -0700

    edit home

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,16 @@ import './Home.css';
 const Home = () => {
   return ( 
     <div className="paragraph">
-      <p style={{ textAlign: 'center', color: 'white' }}>A few projects:</p>
+      <p>Projects I've gone done and did:</p>
       <div className="images">
         <a href="https://optimistic-panini-e9191b.netlify.com/auth.html">
-          <img src="../resources/sauceboss.png" alt="alchemybot thumbnail"/>
+          <img className="project-link" src="../resources/sauceboss.png" alt="alchemybot thumbnail"/>
         </a>
         <a href="https://twitter.com/AlchemyPDXBot">
-          <img src="../resources/alchemybot.png" alt="alchemybot thumbnail"/>
+          <img className="project-link" src="../resources/alchemybot.png" alt="alchemybot thumbnail"/>
         </a>
         <a href="http://star-bbs.github.io">
-          <img src="../resources/starnation.png" alt="alchemybot thumbnail"/>
+          <img className="project-link" src="../resources/starnation.png" alt="alchemybot thumbnail"/>
         </a>
       </div>
     </div>
