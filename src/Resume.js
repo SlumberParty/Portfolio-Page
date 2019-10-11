7b82394b1e6ad7e591b1acc6a28e2c7c5bdfa3df
Claire Cassidy <clacassidy@gmail.com>
2019-10-11 14:56:35 -0700

    resume resizing

--- src/Resume.js
+++ src/Resume.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Resume.css'
 
 const Resume = () => {
   return ( 
     <div className="resume-page">
-
       <img className="resume" src="../resources/Claires-Current-Resume-Jpg.jpg" alt="resume" />
     </div>
    );
