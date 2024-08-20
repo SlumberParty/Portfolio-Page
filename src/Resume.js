8c78c02e9c6a8a6a61222ce70396421a064d3373
Evan Budnick <evanbudnick@Evans-MacBook-Pro.local>
2024-08-20 13:10:01 -0700

    Added dimensions for resume

--- src/Resume.js
+++ src/Resume.js
@@ -3,7 +3,12 @@ import React from 'react';
 import resume from '../src/resources/Claire Cassidy Resume Spring 2024.png'
 
 const Resume = () => {
-   return <img src={resume} alt="resume" />;
+   return <div>
+    <div>hello yes i sneek</div>
+
+    <img src={resume} alt="resume" height={800} width={600}/>;
+    
+    </div>
 }
 
 export default Resume;
