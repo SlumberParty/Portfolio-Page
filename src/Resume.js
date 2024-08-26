5d1c6b5456b6243264a611f734b35bfd34261981
SlumberParty <media@Claires-MacBook-Pro-2014.local>
2024-08-26 11:32:33 -0700

    add padding to resume and led guide page

--- src/Resume.js
+++ src/Resume.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import './Resume.css';
 
 import resume from '../src/resources/Claire Cassidy Resume Spring 2024.png'
 
 const Resume = () => {
-   return <div>
-    <div>hello yes i sneek</div>
-
-    <img src={resume} alt="resume" height={800} width={600}/>;
-    
+    return <div class="resume-page">
+        <img src={resume} alt="resume" height={800} width={600} />;
     </div>
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Portfolio.css';
 
 function Portfolio() {
     return (
-        <div>
-            <iframe width="830" height="740" 
-            src="https://docs.google.com/document/d/180WD4LogMjx8FmKcFc0dmlTEKJ1Xa7dIHlEhSLMOAxA/pub?embedded=true"></iframe>
+        <div class="ledguide-full-page">
+            <iframe width="830" height="740"
+                src="https://docs.google.com/document/d/180WD4LogMjx8FmKcFc0dmlTEKJ1Xa7dIHlEhSLMOAxA/pub?embedded=true"></iframe>
         </div>
     )
 }
