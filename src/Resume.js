cdd4f2e9d0624788736ab147c2b0091c96bb6184
SlumberParty <media@Claires-MacBook-Pro-2014.local>
2024-09-07 07:29:17 -0700

    reworked resume css and js for more flexibility

--- src/Resume.js
+++ src/Resume.js
@@ -4,11 +4,11 @@ import './Resume.css';
 import resume from '../src/resources/Claire Cassidy Resume Fall 2024.png'
 
 const Resume = () => {
-    return <div class="resume-page">
-        {/* <a href="https://drive.google.com/file/d/18kQ1e_GKnSzObzFR2yLho2fEfg2fFSAc/view?usp=drive_link">
-            <p>Click here to view my resume with clickable links!</p>
-          </a> */}
-        <img src={resume} alt="claire cassidy's fall 2024 resume"/>;
+    return <div>
+        <img class="resume-page" src={resume} alt="claire cassidy's fall 2024 resume" />;
+        <div class="resume-link-container">
+            <a href="https://drive.google.com/file/d/18kQ1e_GKnSzObzFR2yLho2fEfg2fFSAc/view?usp=drive_link" class="resume-link">Click here to view my resume with clickable links!</a>
+        </div>
     </div>
 }
 
